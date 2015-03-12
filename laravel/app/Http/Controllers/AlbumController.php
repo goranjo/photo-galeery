2966e90f6aecacc407e33279492b002eba1700e8
Goran <go_jov@yahoo.com>
2015-03-12 04:07:38 +0100

    routes change

--- laravel/app/Http/Controllers/AlbumController.php
+++ laravel/app/Http/Controllers/AlbumController.php
@@ -13,11 +13,11 @@ class AlbumController extends Controller {
     /**
      * @return \Illuminate\View\View
      */
-//    public function index() //index
-//    {
-//        $albums = Album::with('photos')->get();
-//        return view('index', compact('albums'));
-//    }
+    public function index() //index
+    {
+        $albums = Album::with('photos')->get();
+        return view('index', compact('albums'));
+    }
 
     /**
      * @param $id
@@ -26,19 +26,20 @@ class AlbumController extends Controller {
      * album model has_many function 'photos' find through id
      * return to the view 'album' through compact variables 'album', 'albums'
      */
-//    public function getAlbum($id) //show
-//    {
-//
-//        $albums = Album::with('photos')->get();
-//        $album = Album::with('photos')->find($id);
-//        return view('album', compact('album', 'albums'));
-//
-//    }
+    public function getAlbum($id) //show
+    {
+
+        $albums = Album::with('photos')->get();
+        $album = Album::with('photos')->find($id);
+        return view('album', compact('album', 'albums'));
+
+    }
 
     /**
      * @return \Illuminate\View\View
      */
-    public function create(){
+    public function getCreate()
+    {
 
         return view('createalbum');
     }
--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -1,11 +1,10 @@
 <?php
 
 
-//Route::get('/', ['as'=>'index', 'uses'=>'AlbumController@index']);
 Route::post('create', ['as'=> 'create_album', 'uses'=> 'AlbumController@createalbum' ]);
-
+Route::get('/', ['as'=>'index', 'uses'=>'AlbumController@index']);
 //Route::controller('create','AlbumController');
-Route::get('/', ['as'=> 'index', 'uses'=> 'AlbumController@create' ]);
-//Route::get('album{id}', ['as'=>'show_album', 'uses' =>'AlbumController@getAlbum']);
+Route::get('create', ['as'=> 'create_album_form', 'uses'=> 'AlbumController@getCreate' ]);
+Route::get('album{id}', ['as'=>'show_album', 'uses' =>'AlbumController@getAlbum']);
 
 
